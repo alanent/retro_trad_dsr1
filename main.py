cd31b43596aa79179089720d0909c51dff716be2
alanent <a.entem@gmail.com>
2025-02-27 15:33:44 +0100

    first commit

--- main.py
+++ main.py
@@ -9,13 +9,10 @@ from azure.storage.blob import BlobServiceClient
 import logging
 import os
 from dotenv import load_dotenv
-import os
 import io
 from azure.ai.inference import ChatCompletionsClient
 from azure.core.credentials import AzureKeyCredential
-from azure.core.credentials import AzureKeyCredential
 from azure.ai.inference.models import SystemMessage, UserMessage
-import re
 load_dotenv()
 
 app = FastAPI()
@@ -29,23 +26,19 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-
-
-
 @app.on_event("startup")
 async def startup_event():
-
+    # Initialisation du client Azure pour la traduction
     global client
     client = ChatCompletionsClient(endpoint=os.getenv("DSR1_ENDPOINT"), credential=AzureKeyCredential(os.getenv("DSR1_KEY")))
 
     def predict(br, max_tries=2):
-
         tries = 0
         while tries < max_tries:
             try:
                 response = client.complete(
                     messages=[
-                        SystemMessage(content="""
+                        SystemMessage(content=""" 
                         You are an advanced machine translation system specializing in Breton-to-French translation.
                         Your task is to accurately translate Breton text into fluent and natural French while preserving the original meaning, nuances, and cultural context.
                         Ensure that the translations are grammatically correct and stylistically appropriate for the given text.
@@ -67,7 +60,6 @@ async def startup_event():
                 # Vérifier que le résultat est un JSON valide
                 try:
                     json_result = json.loads(result)
-                    # S'assurer que le champ 'translation' est présent
                     if "translation" in json_result:
                         return json_result
                     else:
@@ -82,13 +74,11 @@ async def startup_event():
             print(f"Tentative {tries} échouée, attente de 2 secondes avant de réessayer...")
             time.sleep(2)
 
-        # Si toutes les tentatives échouent, renvoyer un JSON indiquant l'échec
-        return {"translation": None}
-
-
+        return {"translation": None}  # Si échec après max_tries
 
     try:
         logger.info("Démarrage de l'application...")
+
         # Connexion à Azure Blob
         blob_service_client = BlobServiceClient.from_connection_string(os.getenv("STORAGE_CONNECTION_STRING"))
         blob_client = blob_service_client.get_blob_client(container="data", blob="troer-dataset-firebase-adminsdk-fbsvc-58d8f446f7.json")
@@ -105,51 +95,44 @@ async def startup_event():
         db = firestore.client()
         
         logger.info("Connexion à Firebase réussie et à Firestore établie.")
-    except Exception as e:
-        logger.error(f"Erreur de connexion à Firebase : {e}")
-
-    blob_service_client = BlobServiceClient.from_connection_string(os.getenv("STORAGE_CONNECTION_STRING"))
-    blob_client = blob_service_client.get_blob_client(container="data", blob="br_mono.csv")
-    # Téléchargement du fichier et conversion en fichier-like avec io.BytesIO
-    blob_data = blob_client.download_blob().readall()
-    df = pd.read_csv(io.BytesIO(blob_data))  # Utilisation de BytesIO pour convertir en fichier-like
-    logger.info(df.head())
-
-
-
-
-    # Traitement ligne par ligne du DataFrame 
-    for index, row in df.iterrows():
-        # For Serverless API or Managed Compute endpoints
         
-        br_text = row['br']  # Assurez-vous que la colonne 'br' existe dans votre CSV
+        # Téléchargement du fichier CSV contenant les données à traduire
+        blob_client = blob_service_client.get_blob_client(container="data", blob="br_mono.csv")
+        blob_data = blob_client.download_blob().readall()
+        df = pd.read_csv(io.BytesIO(blob_data))  # Utilisation de BytesIO pour convertir en fichier-like
+        logger.info(df.head())
         
-        # Appel à la fonction de prédiction pour traduire le texte
-        result = predict(br_text)
-        
-        if result.get("translation"):
-            fr_translation = result["translation"]
+        # Traitement ligne par ligne du DataFrame
+        for index, row in df.iterrows():
+            br_text = row['br']  # Assurez-vous que la colonne 'br' existe dans votre CSV
+
+            # Appel à la fonction de prédiction pour traduire le texte
+            result = predict(br_text)
             
-            # Enregistrement dans la collection 'to_validate'
-            try:
-                to_validate_ref = db.collection("to_validate").document()
-                to_validate_ref.set({
-                    'br': br_text,
-                    'fr': fr_translation,
-                    'source': "dsr1",
-                    'timestamp': firestore.SERVER_TIMESTAMP  # Vous pouvez ajouter un timestamp pour suivi
-                })
+            if result.get("translation"):
+                fr_translation = result["translation"]
                 
-                logger.info(fr_translation)
+                # Enregistrement dans la collection 'to_validate'
+                try:
+                    to_validate_ref = db.collection("to_validate").document()
+                    to_validate_ref.set({
+                        'br': br_text,
+                        'fr': fr_translation,
+                        'source': "dsr1",
+                        'timestamp': firestore.SERVER_TIMESTAMP
+                    })
+                    logger.info(fr_translation)
 
-                # Mise à jour du compteur dans la collection 'stats'
-                stats_ref = db.collection("stats").document("global")
-                stats_ref.update({
-                    'to_validate': firestore.Increment(1)  # Incrémente le compteur
-                })
-                
-                logger.info(f"Traduction ajoutée à la collection 'to_validate' pour l'entrée {index}.")
-            except Exception as e:
-                logger.error(f"Erreur lors de l'ajout des traductions dans Firestore : {e}")
-        else:
-            logger.error(f"Aucune traduction valide trouvée pour l'entrée {index}.")
+                    # Mise à jour du compteur dans la collection 'stats'
+                    stats_ref = db.collection("stats").document("global")
+                    stats_ref.update({
+                        'to_validate': firestore.Increment(1)  # Incrémente le compteur
+                    })
+                    logger.info(f"Traduction ajoutée à la collection 'to_validate' pour l'entrée {index}.")
+                except Exception as e:
+                    logger.error(f"Erreur lors de l'ajout des traductions dans Firestore : {e}")
+            else:
+                logger.error(f"Aucune traduction valide trouvée pour l'entrée {index}.")
+    except Exception as e:
+        logger.error(f"Erreur lors de l'initialisation de l'application : {e}")
+
